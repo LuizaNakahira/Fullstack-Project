d0e762b06ce537f8d433814fd0ceb61e56d66a7d
Luiza Xavier Nakahira <luizanakahira40@gmail.com>
2024-11-20 17:59:51 -0300

    Alterando para ter o context API

--- explorador-de-paises/src/App.js
+++ explorador-de-paises/src/App.js
@@ -1,86 +1,69 @@
-import React, { useMemo, useState } from 'react';
-import './App.css';
-import SearchForm from './components/SearchForm';
-import CountryCard from './components/CountryCard';
-
-import {Box, CircularProgress, Typography} from '@mui/material';
-import { fetchCountries } from './services/api';
-
+import React, { useContext } from "react";
+import "./App.css";
+import SearchForm from "./components/SearchForm";
+import CountryCard from "./components/CountryCard";
+import { CountriesContext } from "./context/CountriesContext";
+import { Box, CircularProgress, Typography } from "@mui/material";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-
-  const memorizedCountries = useMemo(() => countries, [countries]);
-
-  const handleSearch = (searchParams) => {    
-    const { countryName } = searchParams;
-    
-    if (!countryName) {
-      setErrorMessage('No country found. Fill the country name and click for search.');
-      return;
-    } 
-
-    setIsLoading(true);
-    const delay = 7000;
-    fetchCountries({ countryName })
-    .then((data) => {
-      if (data.length === 0) {
-        setErrorMessage('No country with that name was found.');
-        setCountries([]);
-      } else {
-        setCountries(data);
-        setErrorMessage('');
-      }
-    })
-    .catch((error) => {
-      setErrorMessage('An error occurred. Please try again later.');
-      setCountries([]);
-      console.log("Error when searching for countries", error);
-    })
-    .finally(() => setIsLoading(false), delay)
-  };
-
-  const handleShowAll = () => {
-    setIsLoading(true);
-    const delay = 7000;
-    fetchCountries({})
-      .then((data) => {
-        setCountries(data);
-        setErrorMessage('');
-      })
-      .catch((error) => {
-        console.log("Error when searching for countries", error);
-      })
-      .finally(() => setIsLoading(false), delay);
-  };
+  const { countries, errorMessage, isLoading } = useContext(CountriesContext);
 
   return (
-    <div className='app-background'>
-      <Box sx={{  textAlign: 'center', p: 3, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Typography variant='h2' gutterBottom className='MuiTypography-root title'>Countries Explorer</Typography>
-        <SearchForm 
-          onSearch={handleSearch}
-          onShowAll={handleShowAll}
-        />
+    <div className="app-background">
+      <Box
+        sx={{
+          textAlign: "center",
+          p: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h2" gutterBottom className="MuiTypography-root title">
+          Countries Explorer
+        </Typography>
+        <SearchForm />
         {errorMessage && (
-          <Box sx={{ mt: 2, color: 'error.main', fontWeight: 'bold', border: '1px solid red', padding: 2, borderRadius: 1 }}>
+          <Box
+            sx={{
+              mt: 2,
+              color: "error.main",
+              fontWeight: "bold",
+              border: "1px solid red",
+              padding: 2,
+              borderRadius: 1,
+            }}
+          >
             <Typography color="error">{errorMessage}</Typography>
           </Box>
         )}
         {isLoading ? (
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh' }}> 
-            <CircularProgress size={100} thickness={8} sx={{ color: '#4682B4'}}/>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "50vh",
+            }}
+          >
+            <CircularProgress size={100} thickness={8} sx={{ color: "#4682B4" }} />
           </Box>
         ) : (
-          <Box sx={{  display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: 3}}>
-            {memorizedCountries.map((country) => (
-            <CountryCard key={country.name.commom} country={country}/>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 3,
+            }}
+          >
+            {countries.map((country) => (
+              <CountryCard key={country.name.common} country={country} />
             ))}
           </Box>
-      )}      
-    </Box>
+        )}
+      </Box>
     </div>
   );
 }
--- explorador-de-paises/src/components/SearchForm.js
+++ explorador-de-paises/src/components/SearchForm.js
@@ -1,51 +1,54 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import SearchButton from './SearchButton';
 import ButtonShowAll from './ButtonShowAll';
 import { TextField, Box } from '@mui/material';
+import { CountriesContext } from '../context/CountriesContext';
 
-function SearchForm({ onSearch, onShowAll }) {
-    const [countryName, setCountryName] = useState('');
-    const inputRef = useRef(null);
+function SearchForm() {
+  const { handleSearch, handleShowAll } = useContext(CountriesContext); // Obtém as funções do contexto
+  const [countryName, setCountryName] = useState('');
+  const inputRef = useRef(null);
 
-    useEffect(() => {
-        inputRef.current.focus();
-    }, []);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onSearch({ countryName });
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleSearch({ countryName }); // Chama a função do contexto ao invés da prop
+  };
 
-    return(
-        <Box
-            component="form"
-            onSubmit={handleSubmit}
-            className='ContainerForm'
-            sx={{ gap: 2}}
-        >
-            <TextField 
-                inputRef={inputRef}
-                label='Country Name'
-                variant='outlined'
-                value={countryName}
-                onChange={(event) => setCountryName(event.target.value)}
-                className='searchForm'
-                fullWidth
-                sx={{
-                    '& .MuiInputLabel-root': {
-                    color: '#4682B4',
-                    },
-                    ':-khtml-any-link': {
-                    color: '#ffffff',
-                    },
-                    '& .MuiInputBase-input': {
-                    color: '#ffffff',
-                    },
-                }}
-            />
-            <SearchButton />
-            <ButtonShowAll onShowAll={onShowAll}/>
-        </Box>           
-    );
-};
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      className="ContainerForm"
+      sx={{ gap: 2 }}
+    >
+      <TextField
+        inputRef={inputRef}
+        label="Country Name"
+        variant="outlined"
+        value={countryName}
+        onChange={(event) => setCountryName(event.target.value)}
+        className="searchForm"
+        fullWidth
+        sx={{
+          '& .MuiInputLabel-root': {
+            color: '#4682B4',
+          },
+          ':-khtml-any-link': {
+            color: '#ffffff',
+          },
+          '& .MuiInputBase-input': {
+            color: '#ffffff',
+          },
+        }}
+      />
+      <SearchButton />
+      <ButtonShowAll onShowAll={handleShowAll} />
+    </Box>
+  );
+}
+
 export default SearchForm;
